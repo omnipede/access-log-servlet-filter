3beb3437a3ee324736bdbaf6aad9aad49ff10289
서현규 <omnipede@coinplug.com>
2021-06-22 16:37:08 +0900

    기능: access log 패키지 추가

---
+++ src/main/kotlin/io/omnipede/accesslog/AccessLogger.kt
@@ -0,0 +1,9 @@
+package io.omnipede.accesslog
+
+interface AccessLogger {
+
+    /**
+     * Access log 를 기록하는 인터페이스
+     */
+    fun log(accessLog: AccessLog)
+}
---
+++ src/main/kotlin/io/omnipede/accesslog/CachingRequestWrapper.kt
@@ -0,0 +1,69 @@
+package io.omnipede.accesslog
+
+import org.apache.commons.io.IOUtils
+import java.io.*
+import java.nio.charset.StandardCharsets
+import javax.servlet.ReadListener
+import javax.servlet.ServletInputStream
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletRequestWrapper
+
+
+internal class CachingRequestWrapper(
+    request: HttpServletRequest?
+) : HttpServletRequestWrapper(request) {
+
+    private val contentsAsByteArray: ByteArray
+
+    @Throws(IOException::class)
+    override fun getInputStream(): ServletInputStream {
+        val bis = ByteArrayInputStream(contentsAsByteArray)
+        return CachingInputStream(bis)
+    }
+
+    @Throws(IOException::class)
+    override fun getReader(): BufferedReader {
+        val bis = ByteArrayInputStream(contentsAsByteArray)
+        return BufferedReader(InputStreamReader(bis))
+    }
+
+    fun getBody(maxLength: Int): String {
+        val buf = contentsAsByteArray
+        return if (buf.size > maxLength) "TOO LONG CONTENTS" else String(
+            buf,
+            StandardCharsets.UTF_8
+        )
+    }
+
+    private class CachingInputStream(bis: InputStream) : ServletInputStream() {
+        private val `is`: InputStream = bis
+
+        override fun isFinished(): Boolean {
+            return `is`.available() == 0
+        }
+
+        override fun isReady(): Boolean {
+            return true
+        }
+
+        override fun setReadListener(readListener: ReadListener?) {
+            throw RuntimeException("Not implemented yet")
+        }
+
+        @Throws(IOException::class)
+        override fun read(): Int {
+            return `is`.read()
+        }
+
+        @Throws(IOException::class)
+        override fun read(b: ByteArray): Int {
+            return `is`.read(b)
+        }
+
+    }
+
+    init {
+        val `is`: InputStream = super.getInputStream()
+        contentsAsByteArray = IOUtils.toByteArray(`is`)
+    }
+}
